fba8c0775168b453d652f6d1929de00962ec67c1
dcgbao <giabao515151@gmail.com>
2023-12-09 23:32:49 +0700

    update feature Post to Forum

--- edit-profile.py
+++ edit-profile.py
@@ -17,6 +17,7 @@ class ProfileEdit(unittest.TestCase):
         driver.get("https://school.moodledemo.net/login/index.php")
         driver.find_element(By.ID, "username").send_keys("student")
         driver.find_element(By.ID, "password").send_keys("moodle")
+        time.sleep(2)
         driver.find_element(By.ID, "loginbtn").click()
         time.sleep(2)
 
@@ -26,19 +27,22 @@ class ProfileEdit(unittest.TestCase):
         driver.find_element(By.XPATH, '//*[@id="carousel-item-main"]/a[1]').click()
         time.sleep(2)
 
+
         driver.find_element(By.XPATH, '//*[@id="region-main"]/div/div/div[2]/section[1]/div/ul/li[1]/span/a').click()
         time.sleep(2)
-        fname = driver.find_element(By.ID, "id_firstname")
-        fname.clear()
-        fname.send_keys("Daniel")
-        lname = driver.find_element(By.ID, "id_lastname")
-        lname.clear()
-        lname.send_keys("Williams")
+        firstName = driver.find_element(By.ID, "id_firstname")
+        firstName.clear()
+        firstName.send_keys("Daniel")
+        lastName = driver.find_element(By.ID, "id_lastname")
+        lastName.clear()
+        lastName.send_keys("Williams")
         # email = driver.find_element(By.ID, "id_email")
         # email.clear()
         # email.send_keys("danielwilliams912@demo.com")
+        time.sleep(2)
         driver.find_element(By.ID, "id_submitbutton").click()
         time.sleep(2)
+        
         
     def tearDown(self):
         self.driver.close()
--- post-forum.py
+++ post-forum.py
@@ -2,13 +2,13 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
 
 
 class ForumPost(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
-
 
     def test_post_forum(self):
         driver = self.driver
@@ -17,13 +17,36 @@ class ForumPost(unittest.TestCase):
         driver.get("https://school.moodledemo.net/login/index.php")
         driver.find_element(By.ID, "username").send_keys("student")
         driver.find_element(By.ID, "password").send_keys("moodle")
+        time.sleep(2)
         driver.find_element(By.ID, "loginbtn").click()
+        time.sleep(2)
 
-        # self.assertIn("Python", driver.title)
-        # elem = driver.find_element(By.NAME, "q")
-        # elem.send_keys("pycon")
-        # elem.send_keys(Keys.RETURN)
-        # self.assertNotIn("No results found.", driver.page_source)
+        # Move to Forum
+        # driver.find_element(By.CSS_SELECTOR, "#course-info-container-51-3 > div > div > a").click() # Locating element by CSS Selector
+        driver.find_element(By.XPATH, '//*[@id="course-info-container-51-3"]/div/div/a').click() # Locating element by XPath
+        time.sleep(2)
+        driver.find_element(By.XPATH, '//*[@id="module-944"]/div/div[2]/div[1]/div/div/div/a').click()
+        time.sleep(2)
+
+
+        driver.find_element(By.XPATH, '//*[@id="region-main"]/div[2]/div[1]/div/div[2]/a').click()
+        time.sleep(2)
+        subject = driver.find_element(By.ID, "id_subject")
+        subject.clear()
+        subject.send_keys("Software Testing Method")
+
+        # ------------------------------------------------------------------
+        driver.switch_to.frame(driver.find_element(By.ID, "id_message_ifr"))
+        message = driver.find_element(By.ID, "tinymce")
+        message.clear()
+        message.send_keys("What is the best software testing method?")
+        driver.switch_to.default_content()  # Switch back to the main content
+        # ------------------------------------------------------------------
+
+        time.sleep(2)
+        # driver.find_element(By.XPATH, '/html/body/div[3]/div[4]/div/div[2]/div/section/div[2]/div[2]/div[2]/form/div[4]/div[2]/fieldset/div/div[1]/span/input').click() # Locating element by full XPath
+        driver.find_element(By.ID, "id_submitbutton").click() # Locating element by ID
+        time.sleep(2)
 
 
     def tearDown(self):
